fd1221da2e3d0e86b48a38009b133a17aac123ad
Srishti Mishra <mishrasrishti108@gmail.com>
2026-03-25 14:35:44 +0530

    Create 796. Rotate String.cpp

---
+++ String Manipulation/796. Rotate String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+        else{
+            string s_new = s+s;
+            if(s_new.find(goal)!=string::npos) return true;
+            return false;
+        }
+    }
+};
