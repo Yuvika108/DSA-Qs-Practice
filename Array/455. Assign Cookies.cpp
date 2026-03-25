b85092b20ca40d0d0edc53d8e18011463aadaaac
Srishti Mishra <mishrasrishti108@gmail.com>
2026-03-25 13:57:29 +0530

    Create 455. Assign Cookies.cpp

---
+++ Array/455. Assign Cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n= g.size();    //g=students=greedy
+        int m= s.size();    //s=cookies=substance
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int l=0, r=0;
+
+        while(l<n && r<m){
+            if(g[l]<=s[r]){
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+};
