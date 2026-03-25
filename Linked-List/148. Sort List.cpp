33e96f4796077c5aa33d19c011472da3835808f6
Srishti Mishra <mishrasrishti108@gmail.com>
2026-03-25 14:23:38 +0530

    Add sortList function to sort a linked list
    Implement sorting for a singly-linked list using a vector to store values and sort them before reconstructing the list.

---
+++ Linked-List/148. Sort List.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL) return NULL;
+
+        vector<int> v;
+        ListNode* temp = head;
+
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        sort(v.begin(), v.end());
+
+        ListNode* newHead = new ListNode(v[0]);
+        temp = newHead;
+
+        for(int i=1; i<v.size(); i++){
+            temp->next = new ListNode(v[i]);
+            temp = temp->next;
+        }
+
+        return newHead;
+    }
+};
